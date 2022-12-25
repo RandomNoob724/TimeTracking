d8a1061d78d2ccf3058421ca03303c01da08098a
Emil Persson <emilper1999@gmail.com>
2022-12-25 14:09:43 +0100

    Added Customer model, controller, and views

--- TimeTracking/Data/ApplicationDbContext.cs
+++ TimeTracking/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TimeTracking.Models;
 
 namespace TimeTracking.Data;
 
@@ -9,5 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<TimeTracking.Models.Customer> Customer { get; set; }
 }
 
