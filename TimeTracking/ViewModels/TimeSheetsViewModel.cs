df20eb253bea9ce94cd53682fbb38d54ef345e8b
Emil Persson <emilper1999@gmail.com>
2022-12-28 17:00:47 +0100

    Updated timesheet model and added viewmodel

---
+++ TimeTracking/ViewModels/TimeSheetsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using TimeTracking.Models;
+
+namespace TimeTracking.ViewModels
+{
+    public class TimeSheetsViewModel
+    {
+        public TimeSheet TimeSheet { get; set; }
+        public List<Project> Projects { get; set; }
+    }
+}
