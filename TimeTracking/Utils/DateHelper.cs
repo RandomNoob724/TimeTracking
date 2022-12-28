eaab5236c7b2a704fb69899d0d240b5429fda642
Emil Persson <emilper1999@gmail.com>
2022-12-28 16:58:55 +0100

    Added DateHelper class for working with dates

---
+++ TimeTracking/Utils/DateHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace TimeTracking.Utils
+{
+    public static class DateHelper
+    {
+        public static DateTime FirstDayOfWeek(DateTime date)
+        {
+            DayOfWeek firstDayOfWeek = CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek;
+            int dateOffset = firstDayOfWeek - date.DayOfWeek;
+            DateTime firstDayOfWeekDate = date.AddDays(dateOffset);
+            return firstDayOfWeekDate;
+        }
+
+        public static DateTime LastDayOfWeek(DateTime date)
+        {
+            DateTime lastDayOfWeekDate = FirstDayOfWeek(date).AddDays(6);
+            return lastDayOfWeekDate;
+        }
+    }
+}
